18545906aa7fe7ff44860abfc3b471a89efceb9f
noahmarble <mablenoa@urbandaleschools.com>
2019-01-09 12:55:20 -0600

    almost done with 3.3

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,7 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+semestergrade = int(input("what is your semester grade? "))
+finalgrade = int(input("what is your final grade? "))
+finalweight = int(input("what is your final exam worth? "))
+overall =
