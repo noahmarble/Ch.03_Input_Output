508c432232756e01312d5f0f743b62f1bfbc7a73
noahmarble <mablenoa@urbandaleschools.com>
2019-01-09 12:45:43 -0600

    all done with 3.2

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,8 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 14
 '''
+base1 = int(input("base1: "))
+base2 = int(input("base2: "))
+height = int(input("height: "))
+area = (base1+base2)/2*height
+print("area: ",area)
