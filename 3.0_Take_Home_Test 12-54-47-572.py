f04cefc0fb6469497e9b5bdfb31985a74cfa5fe2
noahmarble <mablenoa@urbandaleschools.com>
2019-02-01 11:15:48 -0600

    final commit

---
+++ 3.0_Take_Home_Test 12-54-47-572.py
@@ -0,0 +1,64 @@
+'''
+HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
+but I will definitely not ask another person except the instructor. Signed: Noah Mable
+1. Write a line of code that will print your name.
+'''
+print("Noah Mable")
+
+'''
+2. Write a program that asks someone for their name and then prints their name to the screen?
+'''
+name = input("what is your name? ")
+print("my name is", name,"yes, im the real", name)
+'''
+3. Predict the output of the following lines of code and then test them? Write down your prediction and the output.
+print (17/9)      prrediction: 1.8888          output: 1.8888888888888888
+print (17//9)                  1                       1
+print (17%9)                   8                       8
+'''
+
+
+'''
+4. Write a program where a user enters a base and height and you print the area of a triangle.
+'''
+base = int(input("enter a base: "))
+height = int(input("enter a height: "))
+area =  base*height/2
+print("area of this triangle is", area)
+'''
+5. Change this program so it works.
+a= "May the Force be with you!"
+print(a)
+'''
+
+
+
+
+'''
+6. Write a line of code that will ask the user for the length of a square's
+side and then print the area of the square
+'''
+
+
+
+'''7. Ask a user for the length of radii of an ellipse and then print its area. 
+Area=pi*a*b where a and b are the lengths of the major radii.
+'''
+
+
+
+'''
+8. Ask a user for moles, volume and temperature of a gas and print out the pressure. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
+volume, and R is the gas constant 8.3144.
+'''
+
+'''
+9. Ask a user for an integer and then print the square root.
+'''
+
+
+'''
+10. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. Ask the user for mass and acceleration
+and then print out the Force on one line and "Get it?" on the next.
+'''
+
---
+++ 3.0_Take_Home_Test copy.py
@@ -0,0 +1,140 @@
+'''
+HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
+but I will definitely not ask another person except the instructor. Signed: noah mable
+
+
+
+1. Write a line of code that will print your name.
+'''
+print("noah mable")
+
+'''
+2. How do you enter a comment in a program?
+
+'''
+#this how you comment
+
+'''
+3. What is the ouput of the following lines of code?
+print (2/3)
+print (2//3)
+0.6666666666666666
+0
+'''
+print (2/3)
+print (2//3)
+
+
+'''
+4. Write a line of code that creates a variable called pi and sets it to an
+appropriate value.
+
+'''
+pi = 3.14
+
+'''
+5. Why does this code not work?
+A=22
+print(a)
+a has not been defined
+'''
+
+
+
+'''
+6. All of the variable names below can be used. But which of these is the best?
+a
+A
+Area
+AREA
+area
+areaOfRectangle
+AreaOfRectangle         this it describes the variable
+'''
+
+
+'''
+7. Which of these variables names are not allowed in Python?
+apple
+Apple
+APPLE
+Apple2
+1Apple                  this
+account number          this
+account_number
+account.number          this
+accountNumber
+account#                this
+'''
+
+
+
+'''
+8. Why does this code not work?
+
+print(a)
+a=45
+a is defined after it is being asked to print
+'''
+
+
+
+'''
+9. Write a line of code that will ask the user for the length of a square's
+side and store the result in a variable. Make sure to convert the value
+to an integer.
+'''
+length = int(input("length of a square: "))
+
+'''
+10. Write a line of code that prints the area of the square, using the
+number the user typed in that you stored in question 9.
+'''
+print("area of a square:", length*length)
+
+
+'''11. Do the same as in questions 9 and 10, but with the formula for the
+area of an ellipse. Area=pi*a*b where a and b are the lengths of the major radii.
+'''
+radious = int(input("radious of an elipse: "))
+radious2 = int(input("second radious of an elipse: "))
+print("area of an ellipse: ", pi*radious*radious2)
+'''
+12. Do the same as in questions 9 and 10, but with a formula to find
+the pressure of a gas. PV=nRT where n is the number of moles, T is the absolute temperature, V is the
+volume, and R is the gas constant 8.3144.
+'''
+#helolo
+
+'''
+13. Explain the mistake in this code:
+
+pi = float(3.14)
+'''
+
+
+
+'''
+14. Assume radius has been defined. This code runs, but isn't very good. Explain the mistake in the following code:
+
+x=3.14
+pi=x
+area=pi*radius**2
+'''
+
+
+'''
+Assuming x and y are already defined, this code runs. But
+something isn't quite right. Explain the mistake in the following code:
+
+a=((x)*(y))
+'''
+
+
+'''
+16. Explain the mistake in the following code:
+
+radius = input(float("Enter the radius:"))
+'''
+
+
